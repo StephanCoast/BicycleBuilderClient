0ecc0d730ba42baf710dd0ebb98ae8d03baab590
axr00 <rauch@th-brandenburg.de>
2022-12-10 09:45:34 +0100

    Refactoring Views, Optimierung
    - Singleton Viewmanager
    - Refactoring alle Controller
    - Pfadänderung CSS Dateien
    - Entfernung Admin/Profil-Button im Builder
    - erste Fade in Tests

--- src/main/java/pf/bb/controller/LoginController.java
+++ src/main/java/pf/bb/controller/LoginController.java
@@ -1,5 +1,7 @@
 package pf.bb.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Alert;
@@ -9,11 +11,24 @@ import java.io.IOException;
 
 public class LoginController {
 
+    ViewManager vm = ViewManager.getInstance();
+    public JFXTextField username;
+    public JFXPasswordField password;
+
     public void authenticate(ActionEvent event) throws IOException {
-        ViewManager vm = new ViewManager();
-        vm.forceView(event, "Dashboard.fxml", "Bicycle Builder - Dashboard", "Dashboard.css");
+        // todo: nach stephanLogin() verschieben beim erfolgreichen Login
+        vm.forceView(event, "Dashboard.fxml", "Bicycle Builder - Dashboard");
+        // Stephan loginTask - zur Trennung
+        stephanLogin();
     }
 
+    //
+    private void stephanLogin() {
+        // alles was du in Login machen möchtest bitte hier rein
+        // benenne die methode wie du willst
+    }
+
+    // todo: eventuell für fehlerhaften Login ebenfalls eine Alert-Box
     public void close() throws IOException {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "", ButtonType.YES, ButtonType.NO);
         alert.setTitle("Bicycle Builder");
--- src/main/java/pf/bb/controller/ViewManager.java
+++ src/main/java/pf/bb/controller/ViewManager.java
@@ -14,126 +14,64 @@ import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.Objects;
 
 public class ViewManager {
 
-    HashSet<JFXDrawer> drawersBuilderSide, drawersDashboard, drawersBuilderBottom;
+    private static final ViewManager instance = new ViewManager();
+    private Stage stage;
+    private Scene scene;
 
     public ViewManager() {
     }
 
-    public void setBuilderSideDrawers(JFXDrawer drawerDefault, JFXDrawer drawerFinish, JFXDrawer drawerCat1, JFXDrawer drawerCat2, JFXDrawer drawerCat3, JFXDrawer drawerCat4, JFXDrawer drawerCat5, JFXDrawer drawerCat6, JFXDrawer drawerCat7, JFXDrawer drawerCat8) {
-        drawersBuilderSide = new HashSet<JFXDrawer>();
-        drawersBuilderSide.add(drawerDefault);
-        drawersBuilderSide.add(drawerFinish);
-        drawersBuilderSide.add(drawerCat1);
-        drawersBuilderSide.add(drawerCat2);
-        drawersBuilderSide.add(drawerCat3);
-        drawersBuilderSide.add(drawerCat4);
-        drawersBuilderSide.add(drawerCat5);
-        drawersBuilderSide.add(drawerCat6);
-        drawersBuilderSide.add(drawerCat7);
-        drawersBuilderSide.add(drawerCat8);
+    public static ViewManager getInstance() {
+        return instance;
     }
 
-    public void setBuilderBottomDrawers(JFXDrawer drawerCats, JFXDrawer drawerData) {
-        drawersBuilderBottom = new HashSet<JFXDrawer>();
-        drawersBuilderBottom.add(drawerCats);
-        drawersBuilderBottom.add(drawerData);
-    }
-
-    public void setDashboardDrawers(JFXDrawer drawerAdmin, JFXDrawer drawerProfile) {
-        drawersDashboard = new HashSet<JFXDrawer>();
-        drawersDashboard.add(drawerAdmin);
-        drawersDashboard.add(drawerProfile);
-    }
-
-    public void forceView(ActionEvent e, String fileName, String title, String cssFileName) throws IOException {
-
-        Stage stage;
-        Scene scene;
-
+    public void forceView(ActionEvent e, String fileName, String title) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/pf/bb/fxml/" + fileName)));
         stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
         scene = new Scene(root);
-        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles/" + cssFileName)).toExternalForm());
         stage.setTitle(title);
-        stage.setMinHeight(720.00);
-        stage.setMinWidth(1280.00);
+        stage.setResizable(true);
+        stage.setScene(scene);
+        stage.show();
+        setFullScreenScene(stage);
+    }
+
+    public void forceLoginView(ActionEvent e, String fileNameLogin, String title) throws IOException {
+        Parent rootLogin = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/pf/bb/fxml/" + fileNameLogin)));
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene = new Scene(rootLogin);
+        stage.setTitle(title);
+        stage.setWidth(300.00);
+        stage.setHeight(250.00);
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+        centerLoginScene(stage);
+    }
+
+    public void forceDrawerView(JFXDrawer drawer, BorderPane category) throws  IOException {
+        drawer.setSidePane(category);
+        drawer.setVisible(true); /* AR: BugFix - JFXButton-Events not passing the JFXDrawer UI */
+        drawer.addEventFilter(MouseDragEvent.MOUSE_DRAGGED, Event::consume); /* AR: BugFix - to prevent Mouse-Dragging on JFXDrawers */
+        drawer.open();
+    }
+
+    private void centerLoginScene(Stage stage) {
+        Rectangle2D sb = Screen.getPrimary().getVisualBounds();
+        stage.setX((sb.getWidth() - stage.getWidth()) / 2);
+        stage.setY((sb.getHeight() - stage.getHeight()) / 2);
+    }
+
+    private void setFullScreenScene(Stage stage) {
         Screen screen = Screen.getPrimary();
         Rectangle2D bounds = screen.getVisualBounds();
         stage.setX(bounds.getMinX());
         stage.setY(bounds.getMinY());
         stage.setWidth(bounds.getWidth());
         stage.setHeight(bounds.getHeight());
-        stage.setResizable(true);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public void forceLoginView(ActionEvent e, String fileName, String title, String cssFileName) throws IOException {
-
-        Stage stage;
-        Scene scene;
-
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/pf/bb/fxml/" + fileName)));
-        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles/" + cssFileName)).toExternalForm());
-        stage.setTitle(title);
-        stage.setWidth(300.00);
-        stage.setHeight(250.00);
-        stage.setResizable(false);
-        stage.setScene(scene);
-        Rectangle2D sb = Screen.getPrimary().getVisualBounds();
-        stage.setX((sb.getWidth() - stage.getWidth()) / 2);
-        stage.setY((sb.getHeight() - stage.getHeight()) / 2);
-        stage.show();
-    }
-
-    public void forceSidebarView(ActionEvent e, JFXDrawer drawer, BorderPane category) throws  IOException {
-        for (JFXDrawer i : drawersBuilderSide) {
-            i.close();
-            i.setVisible(false);
-        }
-        drawer.setSidePane(category);
-        drawer.setVisible(true); /* AR: BugFix - JFXButton-Events not passing the JFXDrawer UI */
-        drawer.addEventFilter(MouseDragEvent.MOUSE_DRAGGED, Event::consume); /* AR: BugFix - to prevent Mouse-Dragging on JFXDrawers */
-        drawer.open();
-    }
-
-    public void forceSidebarInitView(JFXDrawer drawer, BorderPane category) throws  IOException {
-        for (JFXDrawer i : drawersBuilderSide) {
-            i.close();
-            i.setVisible(false);
-        }
-        drawer.setSidePane(category);
-        drawer.setVisible(true); /* AR: BugFix - JFXButton-Events not passing the JFXDrawer UI */
-        drawer.addEventFilter(MouseDragEvent.MOUSE_DRAGGED, Event::consume); /* AR: BugFix - to prevent Mouse-Dragging on JFXDrawers */
-        drawer.open();
-    }
-
-    public void forceBottomDashboardView(ActionEvent e, JFXDrawer drawer, BorderPane category) throws  IOException {
-        for (JFXDrawer i : drawersDashboard) {
-            i.close();
-            i.setVisible(false);
-        }
-        drawer.setSidePane(category);
-        drawer.setVisible(true); /* AR: BugFix - JFXButton-Events not passing the JFXDrawer UI */
-        drawer.addEventFilter(MouseDragEvent.MOUSE_DRAGGED, Event::consume); /* AR: BugFix - to prevent Mouse-Dragging on JFXDrawers */
-        drawer.open();
-    }
-
-    public void forceBottomBuilderView(ActionEvent e, JFXDrawer drawer, BorderPane category) throws  IOException {
-        for (JFXDrawer i : drawersBuilderBottom) {
-            i.close();
-            i.setVisible(false);
-        }
-        drawer.setSidePane(category);
-        drawer.setVisible(true); /* AR: BugFix - JFXButton-Events not passing the JFXDrawer UI */
-        drawer.addEventFilter(MouseDragEvent.MOUSE_DRAGGED, Event::consume); /* AR: BugFix - to prevent Mouse-Dragging on JFXDrawers */
-        drawer.open();
     }
 }
